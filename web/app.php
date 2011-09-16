024df6ed0dc6dc29f09fbfb94bb23f44d443c8c1
Kacper Gunia <cakper@gmail.com>
2011-09-16 20:48:42 +0200

    Umask configuration for filesystem without ACL

--- web/app.php
+++ web/app.php
@@ -1,5 +1,7 @@
 <?php
 
+umask(0002); // For filesystems without ACL. Read more: http://symfony.com/doc/2.0/book/installation.html
+
 require_once __DIR__.'/../app/bootstrap.php.cache';
 require_once __DIR__.'/../app/AppKernel.php';
 //require_once __DIR__.'/../app/AppCache.php';
--- web/app_dev.php
+++ web/app_dev.php
@@ -1,5 +1,7 @@
 <?php
 
+umask(0002); // For filesystems without ACL. Read more: http://symfony.com/doc/2.0/book/installation.html
+
 // this check prevents access to debug front controllers that are deployed by accident to production servers.
 // feel free to remove this, extend it, or make something more sophisticated.
 if (!in_array(@$_SERVER['REMOTE_ADDR'], array(
